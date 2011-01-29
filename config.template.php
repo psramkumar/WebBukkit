5128247da7597a7f07d6fef025b5e4c2eb029d02
Kyle S <kyso1995+g@gmail.com>
2011-01-28 18:27:37 -0600

    Attempt fix two

---
+++ config.template.php
@@ -0,0 +1,21 @@
+<?php
+
+$rooturl = 'http://example.com/bukkitadmin/';
+
+$wwwdir = dirname(realpath($_SERVER['SCRIPT_FILENAME']));
+
+$include = $wwwdir . '/include/';
+
+$minecraft['bin'] = 'bin';
+
+$minecraft['name'] = "Example Server";
+
+$mysql['server'] = 'localhost';
+
+$mysql['user'] = 'root';
+
+$mysql['pass'] = '';
+
+$mysql['db'] = 'bukkitadmin';
+
+?>
